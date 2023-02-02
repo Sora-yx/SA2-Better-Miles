c9d76e37ead7da1f9b631667d851c6d96cad2d01
Sora <sora.x@hotmail.fr>
2023-02-02 19:34:45 +0100

    cleanup

--- SA2-Better-Miles/spin.cpp
+++ SA2-Better-Miles/spin.cpp
@@ -20,14 +20,13 @@ void Miles_CheckSpinAttack(TailsCharObj2* co2M, EntityData1* twp, CharObj2Base* 
 }
 
 
-void Miles_SpinAttack2(playerwk* pwp, taskwk* twp, TailsCharObj2* mCO2)
+void Miles_SpinAttack(playerwk* pwp, taskwk* twp, TailsCharObj2* mCO2)
 {
 	unsigned __int16 curAnim = pwp->mj.reqaction;
 	char pnum = pwp->PlayerNum;
 
 	if (curAnim >= Spin1 && curAnim <= Spin10)
 	{
-
 		AnimationIndex* sa2anim = getCharAnim_r();
 		int curAnim = mCO2->base.AnimInfo.Current;
 		int id = mCO2->base.AnimInfo.Animations[curAnim].AnimNum;
@@ -46,15 +45,15 @@ void Miles_SpinAttack2(playerwk* pwp, taskwk* twp, TailsCharObj2* mCO2)
 		{
 			if (GetAnalog((EntityData1*)twp, (CharObj2Base*)pwp, 0, 0))
 			{
-				unsigned __int16 v4 = ((((4096
+				unsigned __int16 nextAnim = ((((4096
 					- (-njArcTan2((Controllers[pnum].y1 << 8), (Controllers[pnum].x1 << 8)))) >> 13) & 7) + Spin3);
-				if (v4 == pwp->mj.reqaction)
+				if (nextAnim == pwp->mj.reqaction)
 				{
 					TailsAnimationList_R[curAnim].NextAnimation = ((int)pwp->mj.nframe & 1) + Spin1;
 				}
 				else
 				{
-					TailsAnimationList_R[curAnim].NextAnimation = v4;
+					TailsAnimationList_R[curAnim].NextAnimation = nextAnim;
 					Play3DSound_Pos(8200, &twp->pos, 0, 0, 0);
 				}
 
@@ -62,11 +61,10 @@ void Miles_SpinAttack2(playerwk* pwp, taskwk* twp, TailsCharObj2* mCO2)
 				{
 					savePosY = twp->pos.y;
 				}
-				else if (savePosY + 100.0 < twp->pos.y)
+				else if (savePosY + 100.0f < twp->pos.y)
 				{
 					Tails_FlyStart((EntityData1*)twp, (CharObj2Base*)pwp, mCO2);
 				}
-
 			}
 			else
 			{
@@ -85,11 +83,11 @@ void Miles_SpinAttack2(playerwk* pwp, taskwk* twp, TailsCharObj2* mCO2)
 	}
 }
 
-void spinOnFrames(playerwk* pwk, EntityData1* data1, TailsCharObj2* mCo2) {
-
+void spinOnFrames(playerwk* pwk, EntityData1* data1, TailsCharObj2* mCo2)
+{
 	if (pwk->mj.reqaction) 
 	{
-		Miles_SpinAttack2(pwk, (taskwk*)data1, mCo2);
+		Miles_SpinAttack(pwk, (taskwk*)data1, mCo2);
 	}
 	else {
 		data1->Action = 1;
@@ -99,7 +97,12 @@ void spinOnFrames(playerwk* pwk, EntityData1* data1, TailsCharObj2* mCo2) {
 }
 
 void Init_MilesSpin() {
+	
+	if (!isCustomAnim)
+		return;
+
 	WriteData<5>((void*)0x752567, 0x90); //Remove Tail Attack thing
 	WriteData<3>((int*)0x751e56, 0x90);
 	WriteData<11>((int*)0x74ed0a, 0x90);
+	WriteData<5>((int*)0x74D7EE, 0x90);
 }
